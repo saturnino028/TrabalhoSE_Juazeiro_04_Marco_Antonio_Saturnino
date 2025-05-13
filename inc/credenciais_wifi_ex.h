51a750c87c35ab06234bda84ada2ae586be9eeb1
saturnino028 <marcossaturnino028@gmail.com>
2025-05-13 01:48:53 -0300

    Informações adicionais funcionamento

--- inc/credenciais_wifi_ex.h
+++ inc/credenciais_wifi_ex.h
@@ -1,3 +1,6 @@
-// Credenciais WIFI - Tome cuidado se publicar no github!
+/*
+Credenciais WIFI - Para funcionar, basta remover os caracteres: "_ex" do nome deste arquivo e colocar os dados
+da sua rede
+*/ 
 #define WIFI_SSID "O nome da sua rede WiFi"
 #define WIFI_PASSWORD "A senha da sua rede WiFi"
